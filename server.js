5f92de4a6eb514206e2af0c91a14eb3f94796938
Unarine <unarinedev@gmail.com>
2026-03-12 13:56:21 +0200

    deploy version

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.post("/layout-min.js", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log("Server running on port " + PORT);
 });
